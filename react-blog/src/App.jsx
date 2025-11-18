ab25e610469c54913286022fa5134ca005348eed
jimishgenai <jimishgenai@gmail.com>
2025-11-18 22:05:22 +0530

    added state

--- react-blog/src/App.jsx
+++ react-blog/src/App.jsx
@@ -1,15 +1,19 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import User from './User';
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
+  const [counter, setCounter] = useState(0);
 
   return (
     <>
-      <h1>Code step by step</h1>
       <h1>Hello React 19</h1>
+      <h1>Counter Value: {counter}</h1>
+      <button onClick={() => setCounter(counter + 1)}>Increase counter</button>
+      <User />
     </>
   )
 }
---
+++ react-blog/src/User.jsx
@@ -0,0 +1,5 @@
+function User() {
+  return <h1>User Components</h1>;
+}
+
+export default User;
